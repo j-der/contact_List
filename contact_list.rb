fe975a87ef93d184c1b80c6cec0d61a8f14746e2
Jas Der <jasmineder@gmail.com>
2016-01-15 19:10:05 +0000

    made some corrections

--- contact_list.rb
+++ contact_list.rb
@@ -3,6 +3,37 @@ require_relative 'contact'
 # Interfaces between a user and their contact list. Reads from and writes to standard I/O.
 class ContactList
 
-  # TODO: Implement user interaction. This should be the only file where you use `puts` and `gets`.
+  puts "Here is a list of available commands:"
+  puts "new - Create a new contact"
+  puts "list - List all contacts"
+  puts "show - Show a contact"
+  puts "search - Search contacts"
+
+  input = gets.chomp!
+
+  case input
+  when "new"
+    puts "Enter the full name"
+    name = gets.chomp!
+
+    puts "Enter the email address"
+    email = gets.chomp!
+
+    Contact.create(name, email)
+    puts "#{name}, #{email} has been added."
+
+  when "list"
+    puts Contact.all
+
+  when "show"
+    puts "Please enter an ID number to find a contact."
+    id = gets.chomp.to_i
+    puts Contact.find(id)
+
+  when "search"
+    puts "Please enter a name or email"
+    term = gets.chomp.to_s
+    puts Contact.search(term)
+  end
 
 end
